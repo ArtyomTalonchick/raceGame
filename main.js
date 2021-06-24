a4c687ce93e2c8b3aa3ada28ef889db36c508c7a
Артем Таланец <artsiom.talanets@soft-fx.lv>
2021-06-24 12:59:07 +0300

    Third lesson

--- main.js
+++ main.js
@@ -10,20 +10,29 @@ const keys = {
 const setting = {
     start: false,
     score: 0,
-    speed: 3,
+    speed: 0,
     x: null,
     y: null,
     traffic: 3,
 };
 
-const $score = document.querySelector(".score");
-const $gameStart = document.querySelector(".game__start");
+const $audioGame = document.querySelector("#audio-game");
+const $audioOvertaking = document.querySelector("#audio-overtaking");
+const $audioBreaking = document.querySelector("#audio-breaking");
+const $score = document.querySelector(".game__header-score");
+const $record = document.querySelector(".game__header-record");
+const $gameStartBtnsContainer = document.querySelector(".game__buttons");
+const $gameStartBtns = document.querySelectorAll(".game__button");
 const $gameArea = document.querySelector(".game__area");
 const $car = document.createElement("div");
 $car.classList.add("car");
+$audioGame.volume = .5;
+let stopOvertakingTimeoutId = null;
 
+const updateRecord = () => $record.textContent = `Record: ${localStorage.getItem("bestScore") || 0}`;
 const getQuantityElements = height => Math.ceil(document.documentElement.clientHeight / height) + 1;
 const getRandomLeft = width => `${Math.random() * ($gameArea.offsetWidth - width)}px`;
+updateRecord();
 
 
 const moveRoad = () => {
@@ -36,7 +45,22 @@ const moveRoad = () => {
 }
 
 const moveEnemy = () => {
+    const carRect = $car.getBoundingClientRect();
     document.querySelectorAll(".enemy").forEach($enemy => {
+
+        const enemyRect = $enemy.getBoundingClientRect();
+
+        if (carRect.bottom >= enemyRect.top && carRect.top <= enemyRect.bottom) {
+            $audioOvertaking.play();
+            clearTimeout(stopOvertakingTimeoutId);
+            stopOvertakingTimeoutId = setTimeout(() => $audioOvertaking.pause(), 1000);
+
+            if (carRect.left <= enemyRect.right && carRect.right >= enemyRect.left) {
+                $audioBreaking.play();
+                endGame();
+            }
+        }
+
         $enemy.y += setting.speed / 2;
         $enemy.style.top = `${$enemy.y}px`;
 
@@ -52,6 +76,8 @@ const playGame = () => {
 
     moveRoad();
     moveEnemy();
+    setting.score += setting.speed;
+    $score.textContent = `Score: ${setting.score}`;
 
     setting.x += (keys.ArrowRight - keys.ArrowLeft) * setting.speed
     setting.y += (keys.ArrowDown - keys.ArrowUp) * setting.speed
@@ -67,8 +93,16 @@ const playGame = () => {
     requestAnimationFrame(playGame);
 };
 
-const startGame = () => {
-    $gameStart.classList.add("_hide");   
+const startGame = e => {
+    setting.speed = +e.currentTarget.dataset.speed || 2;
+    $audioGame.play();
+    $gameStartBtnsContainer.classList.add("_hide");
+    $gameArea.innerHTML = "";
+    $car.style.left = "calc(50% - 25px)";
+    $car.style.top = "auto";
+    $car.style.bottom = "10px";
+
+    Object.keys(keys).forEach(name => keys[name] = false);
 
     for (let i = 0; i < getQuantityElements(100); i++) {
         const $line = document.createElement("div");
@@ -88,22 +122,40 @@ const startGame = () => {
     }
 
     setting.start = true;
+    setting.score = 0;
     $gameArea.appendChild($car);
     setting.x = $car.offsetLeft;
     setting.y = $car.offsetTop;
     requestAnimationFrame(playGame);
 };
 
+
+const endGame = () => {
+    setting.start = false;
+    $gameStartBtnsContainer.classList.remove("_hide");
+    
+    $audioGame.pause();
+    $audioOvertaking.pause();
+
+    const bestScore = localStorage.getItem("bestScore") || 0;
+    if (setting.score > bestScore) {
+        localStorage.setItem("bestScore", setting.score);
+        alert(`You have broken the record!\nOld result: ${bestScore}\nYour score: ${setting.score}`);
+        updateRecord();
+    }
+
+};
+
 const startRun = e => {
     e.preventDefault();
-    keys[e.key] = true;
+    e.key in keys && (keys[e.key] = true);
 };
 
 const stopRun = e => {
     e.preventDefault();
-    keys[e.key] = false;
+    e.key in keys && (keys[e.key] = false);
 };
 
-$gameStart.addEventListener("click", startGame);
+$gameStartBtns.forEach(btn => btn.addEventListener("click", startGame));
 document.addEventListener("keydown", startRun);
 document.addEventListener("keyup", stopRun);
